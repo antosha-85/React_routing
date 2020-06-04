c6e819bb47d4c84a3649f08d5b0127b8150baf21
Anton Smirnov <antosha-85@yandex.ru>
2020-06-03 22:33:00 -0600

    Added a new way of rendering using component inside Route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -19,9 +19,11 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path='/'exact render={()=><h1>Home</h1>}/>
-        <Route path='/' render={()=><h1>Home2</h1>}/>
-        <Posts />
+        {/* <Route path='/'exact render={()=><h1>Home</h1>}/>
+        <Route path='/' render={()=><h1>Home2</h1>}/> */}
+        <Route path='/' exact component={Posts}/>
+        <Route path='/' render={()=><Posts/>}/>
+         {/* <Posts /> */}
       </div>
     );
   }
