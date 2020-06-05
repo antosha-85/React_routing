eab9f9510dd0097073047038e8ffcefc7205a24c
Anton Smirnov <antosha-85@yandex.ru>
2020-06-04 23:16:19 -0600

    Added Switch to my app

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import Posts from "./Posts/Posts";
-import NewPost from './NewPost/NewPost'
-import FullPost from './FullPost/FullPost'
+import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
 
-import {Route, NavLink} from 'react-router-dom'
+import { Route, NavLink, Switch } from "react-router-dom";
 import "./Blog.css";
 
 class Blog extends Component {
@@ -14,33 +14,42 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <NavLink 
-                exact 
-                to="/"
-                activeClassName='my_active'
-                activeStyle={{
-                  color: '#fa923f',
-                  textDecoration: 'underline'
-                }}>Home</NavLink>
+                <NavLink
+                  exact
+                  to="/"
+                  activeClassName="my_active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Home
+                </NavLink>
               </li>
               <li>
-                <NavLink to={{
-                  // pathname: this.props.match.url + '/new-post',
-                  pathname: '/new-post',
-                  hash: '#submit',
-                  search: '?quick-submit=true'
-                }}>New Post</NavLink>
+                <NavLink
+                  to={{
+                    // pathname: this.props.match.url + '/new-post',
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true",
+                  }}
+                >
+                  New Post
+                </NavLink>
               </li>
             </ul>
           </nav>
         </header>
         {/* <Route path='/'exact render={()=><h1>Home</h1>}/>
         <Route path='/' render={()=><h1>Home2</h1>}/> */}
-        <Route path='/' exact component={Posts}/>
-        <Route path='/new-post' component={NewPost}/>
-        <Route path='/:id' exact component={FullPost} {...this.props} hi='hi'/>
+        <Switch>
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} hi="hi" />
+        </Switch>
         {/* <Route path='/' render={()=><Posts/>}/> */}
-         {/* <Posts /> */}
+        {/* <Posts /> */}
       </div>
     );
   }
