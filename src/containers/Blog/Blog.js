4a4238dda714ebb75d7ae8e31d1ae79baaf6fd15
Anton Smirnov <antosha-85@yandex.ru>
2020-06-06 00:04:41 -0600

    Added lazy loading(asynchronous) by creating a high order component and import it asynchronously to create a partial bundle

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
+// import NewPost from "./NewPost/NewPost";
+import AsyncComponent from '../../hoc/asyncComponent'
 // import FullPost from "./FullPost/FullPost";
 
 import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 import "./Blog.css";
 
+const AsyncNewPost = AsyncComponent(()=> {
+  return import("./NewPost/NewPost");
+})
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   }
   render() {
     return (
@@ -47,9 +51,9 @@ class Blog extends Component {
         {/* <Route path='/'exact render={()=><h1>Home</h1>}/>
         <Route path='/' render={()=><h1>Home2</h1>}/> */}
         <Switch>
-          {this.state.auth ? <Route path="/new-post" component={NewPost} />: null}
+          {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} />: null}
           <Route path="/posts" component={Posts} />
-          <Route render={()=> <h1>Not found</h1>}
+          <Route render={()=> <h1>Not found</h1>}/>
           {/* <Redirect from="/" to="/posts" /> */}
           {/* <Route path="/" component={Posts} /> */}
           {/* <Route path="/:id" exact component={FullPost} hi="hi" /> */}
