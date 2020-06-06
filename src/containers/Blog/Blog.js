32d8b9af8c2869891755519c7759253e7455046d
Anton Smirnov <antosha-85@yandex.ru>
2020-06-05 23:06:50 -0600

    Added redirect inside Switch, now if I go to / it will be redirected to /posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
 // import FullPost from "./FullPost/FullPost";
 
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 import "./Blog.css";
 
 class Blog extends Component {
@@ -46,6 +46,8 @@ class Blog extends Component {
         <Switch>
           <Route path="/new-post" component={NewPost} />
           <Route path="/posts" component={Posts} />
+          <Redirect from="/" to="/posts" />
+          {/* <Route path="/" component={Posts} /> */}
           {/* <Route path="/:id" exact component={FullPost} hi="hi" /> */}
         </Switch>
         {/* <Route path='/' render={()=><Posts/>}/> */}
