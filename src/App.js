5578a1db5d1645540e34e9eca536702c17f9576e
Anton Smirnov <antosha-85@yandex.ru>
2020-06-07 00:03:14 -0600

    Added basename to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, Fragment } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Blog from "./containers/Blog/Blog";
 
 class App extends Component {
+  state = {
+    showPosts: false
+  }
+
+  modeHandler = () => {
+    this.setState(prevState=> {
+      return {showPosts: !prevState.showPosts}
+    })
+  }
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Blog />
-        </div>
-      </BrowserRouter>
+      // <BrowserRouter basename='/'>
+      //   <div className="App">
+      //     <Blog />
+      //   </div>
+      // </BrowserRouter>
+      <React.Fragment>
+      <button onClick={this.modeHandler}>Toggle</button>
+      {this.state.showPosts ? (<Suspense fallback={<div>Loading...</div>}>Post</Suspense>) : <p>User</p>}
+</React.Fragment>
     );
   }
 }
